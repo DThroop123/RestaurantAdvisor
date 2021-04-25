dd0c2b8825d010e59d252d7be987518916078af0
jwerner54 <jacob.f.werner@comcast.net>
2021-04-25 14:47:50 -0500

    Restaurant page updates

---
+++ src/main/java/com/example/demo/controller/RestaurantsController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.domain.SumRestaurant;
+import com.example.demo.service.SumRestaurantService;
+
+// Controller for dynamic pages in the orders page 
+
+@Controller
+public class RestaurantsController {
+
+	@Autowired
+	SumRestaurantService sumRestService;
+
+	@RequestMapping(value = "/restaurants", method = RequestMethod.GET)
+	public String restaurantsDisplay(Model model) {
+		
+		List<SumRestaurant> sumRest = new ArrayList<SumRestaurant>();
+		sumRest = sumRestService.getSumRestaurantList();
+		
+		// console test
+//		for (SumRestaurant r : sumRest)
+//			System.out.println(r);
+//		
+		model.addAttribute("sumRestList", sumRest);
+		
+		return "Restaurants";
+
+	}
+
+}
